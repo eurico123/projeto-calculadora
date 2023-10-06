893e65624b4a843e745d387c434419b9b8bf29de
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-06 13:27:08 -0300

    criei calculadora para treinar js

---
+++ elements/script.js
@@ -0,0 +1,22 @@
+function inserir(num){
+    var numero = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = numero + num
+}
+
+function limpar(){
+    var res = document.getElementById('res')
+    res.innerHTML = ''
+}
+
+function voltar(){
+    var res = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = res.substring(0, res.length -1)
+
+}
+
+function calcular(){
+    var res = document.getElementById('res').innerHTML
+    if (res){
+        document.getElementById('res').innerHTML = eval(res)
+    }
+}
